b8a9a42a9970a47d583506777199c856b734d3e8
MahimkO <maxim-ka@bk.ru>
2022-05-22 13:37:19 +0300

    fixed links

--- src/components/WorkBox/WorkBox.tsx
+++ src/components/WorkBox/WorkBox.tsx
@@ -2,20 +2,18 @@ import type { Work } from '../../data/works';
 
 import './WorkBox.scss';
 
-export function WorkBox({id, name, description, image, url} : Work) {  
-  const handleClick = (url : string) => {
-    window.location.replace(url);
-  };
-  
-  return ( 
-    <div onClick={() => handleClick(url)} key={id} className='WorkBox__container'>
-      <div className='WorkBox__image_container'>
-        <img className='WorkBox__image' src={image} alt=''></img>
+export function WorkBox({id, name, description, image, url} : Work) {    
+  return (
+    <a href={url} rel='noreferrer' target='_blank' key={id} >
+      <div className='WorkBox__container'>
+        <div className='WorkBox__image_container'>
+          <img className='WorkBox__image' src={image} alt=''></img>
+        </div>
+        <div className='WorkBox__info_container'>
+          <p className='WorkBox__title'>{name}</p>
+          <p className='WorkBox__description'>{description}</p>
+        </div>
       </div>
-      <div className='WorkBox__info_container'>
-        <p className='WorkBox__title'>{name}</p>
-        <p className='WorkBox__description'>{description}</p>
-      </div>
-    </div>
+    </a>
   );
 }
